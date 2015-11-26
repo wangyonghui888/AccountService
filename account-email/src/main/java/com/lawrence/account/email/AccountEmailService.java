6e4cc67e3622f90eed783f4f32b90f9d72883949
Lawrence Chen <lawrence.scv@gmail.com>
2015-11-26 20:17:27 +0800

    minor change

--- account-email/src/main/java/com/lawrence/account/email/AccountEmailService.java
+++ account-email/src/main/java/com/lawrence/account/email/AccountEmailService.java
@@ -2,5 +2,5 @@ package com.lawrence.account.email;
 
 public interface AccountEmailService {
 
-	public void sendMail(String to, String title, String htmlContent) throws AccountEmailException;
+	public void sendMail(String to, String subject, String htmlContent) throws AccountEmailException;
 }
